fa1a02d84b261edfb7065fee5ae7a28e31cb6fb8
bhupalpsl <bhupal_pamuluri@persistent.com>
2024-11-07 20:37:53 +0530

    creating CD-workflow

--- lambda_function.py
+++ lambda_function.py
@@ -8,7 +8,7 @@ def lambda_handler(event, context):
     response = {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "Hello from Lambda!",
+            "message": "Hello from Lambda! this is bhupal",
             "input": event
         })
     }
