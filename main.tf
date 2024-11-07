d7cc7cb9ccc8a83bd3bd42d9a82a7b37d002ab7d
bhupalpsl <bhupal_pamuluri@persistent.com>
2024-11-07 20:27:22 +0530

    creating CD-workflow

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
-module "lambda" {
-  source  = "terraform-aws-modules/lambda/aws"
-  version = "7.14.0"
+
+module "my_lambda_module" {
+  source = "git::https://github.com/bhupal/lambda-module.git//?ref=main"
+  # Define module variables
+  function_name = "my_lambda_function"
+  runtime       = "python3.8"
+  handler       = "index.handler"
 }
