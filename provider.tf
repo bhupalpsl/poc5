0e36e7a69afedb80a4a0b732a505b8bd2734b2cd
bhupalpsl <bhupal_pamuluri@persistent.com>
2024-11-11 19:05:42 +0530

    updating workflow

--- provider.tf
+++ provider.tf
@@ -12,26 +12,3 @@ terraform {
 }
 
 }
-
-
-
-resource "aws_s3_bucket_object" "object" {
-  bucket =  var.lambda_s3_bucket
-  key    =  var.lambda_s3_key
-  source =  "lambda_function.zip"                         #"path/to/file"     
-
-  # The filemd5() function is available in Terraform 0.11.12 and later
-  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
-  # etag = "${md5(file("path/to/file"))}"
-   etag = filemd5("lambda_function.zip")
-}
-
-
-output "s3_object_etag" {
-  value = aws_s3_bucket_object.object.etag
-}
-
-
-output "s3_object_version" {
-  value = aws_s3_bucket_object.object.version_id
-}
