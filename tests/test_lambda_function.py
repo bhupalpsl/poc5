d03d2c014cea52cf4f84e2e9a2823dafa67ef4be
bhupalpsl <bhupal_pamuluri@persistent.com>
2024-12-05 19:04:34 +0530

    adding unittesting

--- tests/test_lambda_function.py
+++ tests/test_lambda_function.py
@@ -1,9 +1,14 @@
-import pytest
-from lambda_function import lambda_handler
-
-def test_handler():
-    event = {}
-    context ={}
-    response = lambda_handler(event, context)
-    assert response['statusCode']== 200
-    assert response['body']== 'Hello from Lambda! this is bhupal'
+import sys
+import os
+# Add the `terraform` directory to PYTHONPATH dynamically
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+import lambda_function  # Import the Lambda function directly
+def test_lambda_handler():
+    # Mock event and context
+    mock_event = {"key": "value"}
+    mock_context = {}
+    # Call the Lambda handler
+    response = lambda_function.lambda_handler(mock_event, mock_context)
+    # Assert the response
+    assert response["statusCode"] == 200
+    assert response["body"] == "Hello from Lambda! this is bhupal"    
